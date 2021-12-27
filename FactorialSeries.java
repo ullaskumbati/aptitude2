abbf2b2c2951077e1e3f1f08faddab70199399db
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-27 23:45:28 +0530

    added program that prints factorial in full series

---
+++ FactorialSeries.java
@@ -0,0 +1,10 @@
+class FactorialSeries{
+	public static void main (String [] args){
+	long facto=1;
+	long n=50;
+		for(int i=1;i<=n;i++){
+			facto=facto*i;
+				System.out.println(facto);
+		}
+	}
+}
