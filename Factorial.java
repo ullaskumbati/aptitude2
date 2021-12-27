7f219979513ac3edb90ed3cdaf8acab2ae834b0b
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-27 23:44:09 +0530

    added programme that prints factorial

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+class Factorial{
+	public static void main (String[]args){
+		int fact=1,n=5;
+			
+			System.out.println("Factorial of 5 is");		
+			for(int i=1;i<=n;i=i+1){
+			fact=fact*i;
+			}
+				System.out.println(fact);
+	}
+}
